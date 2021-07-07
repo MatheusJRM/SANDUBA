ebb7a6c5fffd13f51ae5bf7000062c5cab5fb0ef
dalmeidarj <f.dalmeidarj@gmail.com>
2021-07-07 11:07:06 -0300

    Atualizao cadastro

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -1,38 +1,82 @@
-import React from "react";
-import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import React, { useState } from 'react';
+import { TouchableOpacity, StyleSheet, Text, View, TextInput } from 'react-native';
 
-const Cadastro = ({ navigation }) => {
+export default function Forms({navigation}) {
+  const [nome, setNome] = useState('');
+  const [descricao, setDescricao] = useState('');
+
   return (
-    <View style={styles.center}>
-      <Text>Página Cadastro</Text>
+    <View style={styles.inputs}>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome"
+        autoCorrect={false}
+        onChangeText={nome => setNome(nome)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Descricao"
+        autoCorrect={false}
+        onChangeText={descricao => setDescricao(descricao)}
+      />
+
+      <TouchableOpacity style={styles.botao}>
+        <Text style={styles.textoBotao}>Cadastrar</Text>
+      </TouchableOpacity>
       <TouchableOpacity
         style={styles.btn}
         onPress={() => navigation.navigate("Home")}>
-        <Text style={styles.txtBtn}>Ir para a Página Home</Text>
+        <Text style={styles.textoBotao}>Ir para a Home</Text>
       </TouchableOpacity>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
+  input: {
+    backgroundColor: '#FFF',
+    width: '70%',
+    color: '#222',
+    fontSize: 20,
+    borderRadius: 10,
+    padding: 10,
+    marginLeft: "15%",
+    marginTop: 20,
+    borderColor: 'black',
+    borderWidth: 1
   },
-  btn: {
+  inputs: {
+    marginTop: 40,
+
+  },
+  botao: {
+    backgroundColor: '#ff6000',
+    width: '70%',
+    height: 45,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    elevation: 3,
-    backgroundColor: '#ff0000',
+    borderRadius: 20,
+    borderColor: "#FF4500",
+    borderWidth: 1.2,
+    marginLeft: "15%",
+    marginTop: 20
   },
-  txtBtn: {
-    color: '#fff',
-  }
-});
+  btn: {
+    backgroundColor: '#ff6000',
+    width: '70%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    borderColor: "#FF4500",
+    borderWidth: 1.2,
+    marginLeft: "15%",
+    marginTop: 20
+  },
+  textoBotao: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: "bold"
 
-export default Cadastro;
+  }
+})
---
+++ src/pages/listagem/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+const Lista = () => {
+
+    return (
+        <SafeAreaView>
+
+        </SafeAreaView>
+    )
+}
+export default Lista;
